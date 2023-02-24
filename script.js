428a000dff386d3f5df4e424569deeb72985769e
Winfredo <winfrednukpe2002@gmail.com>
2023-02-24 00:40:13 +0000

    Changes

--- script.js
+++ script.js
@@ -88,6 +88,117 @@ const quizInfo = [
     d: "Cairo",
     correct: "d"
   },
+    {
+      question: "Who invented the telephone?",
+      a: "Alexander Graham Bell",
+      b: "Thomas Edison",
+      c: "Nikola Tesla",
+      d: "Guglielmo Marconi",
+      correct: "a"
+    },
+    {
+      question: "Who invented the light bulb?",
+      a: "Thomas Edison",
+      b: "Nikola Tesla",
+      c: "Alexander Graham Bell",
+      d: "Guglielmo Marconi",
+      correct: "a"
+    },
+    {
+      question: "Who invented the airplane?",
+      a: "Wilbur and Orville Wright",
+      b: "Leonardo da Vinci",
+      c: "Benjamin Franklin",
+      d: "Albert Einstein",
+      correct: "a"
+    },
+    {
+      question: "Who invented the steam engine?",
+      a: "James Watt",
+      b: "Thomas Edison",
+      c: "Eli Whitney",
+      d: "Benjamin Franklin",
+      correct: "a"
+    },
+    {
+      question: "Who invented the cotton gin?",
+      a: "Eli Whitney",
+      b: "James Watt",
+      c: "Thomas Edison",
+      d: "Nikola Tesla",
+      correct: "a"
+    },
+    {
+      question: "Who invented the printing press?",
+      a: "Johannes Gutenberg",
+      b: "Benjamin Franklin",
+      c: "Thomas Edison",
+      d: "Nikola Tesla",
+      correct: "a"
+    },
+    {
+      question: "Who invented the computer?",
+      a: "Charles Babbage",
+      b: "Steve Jobs",
+      c: "Bill Gates",
+      d: "Mark Zuckerberg",
+      correct: "a"
+    },
+    {
+      question: "Who invented the internet?",
+      a: "Tim Berners-Lee",
+      b: "Bill Gates",
+      c: "Steve Jobs",
+      d: "Mark Zuckerberg",
+      correct: "a"
+    },
+    {
+      question: "Who invented the television?",
+      a: "Philo Farnsworth",
+      b: "John Logie Baird",
+      c: "Thomas Edison",
+      d: "Nikola Tesla",
+      correct: "a"
+    },
+    {
+      question: "Who invented the telephone switchboard?",
+      a: "A.S. Erickson",
+      b: "Alexander Graham Bell",
+      c: "Thomas Edison",
+      d: "Guglielmo Marconi",
+      correct: "a"
+    },
+    {
+      question: "Who invented the microwave oven?",
+      a: "Percy Spencer",
+      b: "Albert Einstein",
+      c: "Benjamin Franklin",
+      d: "Nikola Tesla",
+      correct: "a"
+    },
+    {
+      question: "Who invented the refrigerator?",
+      a: "Carl von Linde",
+      b: "Albert Einstein",
+      c: "Thomas Edison",
+      d: "Guglielmo Marconi",
+      correct: "a"
+    },
+    {
+      question: "Who invented the light-emitting diode (LED)?",
+      a: "Nick Holonyak Jr.",
+      b: "Thomas Edison",
+      c: "Nikola Tesla",
+      d: "Guglielmo Marconi",
+      correct: "a"
+    },
+    {
+      question: "Who invented the battery?",
+      a: "Alessandro Volta",
+      b: "Thomas Edison",
+      c: "Nikola Tesla",
+     
+  },
   {
     question: " What is the capital of South Korea?",
     a: "Seoul",
@@ -176,6 +287,111 @@ const quizInfo = [
     d: "Medina",
     correct: "b"
   },
+  {
+      question: "What is the fastest land animal?",
+      a: "Lion",
+      b: "Leopard",
+      c: "Cheetah",
+      d: "Gazelle",
+      correct: "c"
+   },
+   {
+      question: "What is the largest mammal in the world?",
+      a: "Giraffe",
+      b: "Hippopotamus",
+      c: "Elephant",
+      d: "Whale",
+      correct: "d"
+    },
+    {
+      question: "What is the only continent where penguins live naturally?",
+      a: "North America",
+      b: "Europe",
+      c: "Antarctica",
+      d: "Australia",
+      correct: "c"
+    },
+    {
+      question: "What is the only bird that can fly backwards?",
+      a: "Penguin",
+      b: "Hummingbird",
+      c: "Ostrich",
+      d: "Eagle",
+      correct: "b"
+    },
+    {
+      question: "What is the smallest mammal in the world?",
+      a: "Mouse",
+      b: "Shrew",
+      c: "Rat",
+      d: "Bat",
+      correct: "b"
+    },
+    {
+      question: "What is the largest land animal?",
+      a: "Giraffe",
+      b: "Elephant",
+      c: "Hippopotamus",
+      d: "Rhino",
+      correct: "b"
+    },
+    {
+      question: "What is the fastest bird?",
+      a: "Peregrine Falcon",
+      b: "Bald Eagle",
+      c: "Osprey",
+      d: "Golden Eagle",
+      correct: "a"
+    },
+    {
+      question: "What is the most venomous snake in the world?",
+      a: "Black Mamba",
+      b: "King Cobra",
+      c: "Rattlesnake",
+      d: "Inland Taipan",
+      correct: "d"
+    },
+    {
+      question: "What is the only mammal that can truly fly?",
+      a: "Flying Squirrel",
+      b: "Bat",
+      c: "Bird",
+      d: "Penguin",
+      correct: "b"
+    },
+    {
+      question: "What is the largest fish in the world?",
+      a: "Great White Shark",
+      b: "Whale Shark",
+      c: "Manta Ray",
+      d: "Hammerhead Shark",
+      correct: "b"
+    },
+    {
+      question: "What is the tallest animal?",
+      a: "Giraffe",
+      b: "Horse",
+      c: "Elephant",
+      d: "Camel",
+      correct: "a"
+    },
+    {
+      question: "What is the only mammal that can walk on water?",
+      a: "Otter",
+      b: "Seal",
+      c: "Platypus",
+      d: "Kangaroo",
+      correct: "c"
+    },
+    {
+      question: "What is the slowest mammal in the world?",
+      a: "Sloth",
+      b: "Koala",
+      c: "Hedgehog",
+      d: "Gorilla",
+      correct: "a"
+    },
+   
 ];
 const quiz = document.getElementById('quiz');
 const AnswerEl = document.querySelectorAll('.answer');
